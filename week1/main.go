7f686773fe3edfbdccee74d88041a3d5185541f2
Phuoc Le <phuoc.le@groovetechnology.com>
2019-07-19 09:19:24 +0700

    Week 1 - Assignment

---
+++ week1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		x, y, op, err := parseInput(scanner.Text())
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		calc(x , y, op)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
