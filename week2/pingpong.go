898eee26b2e49f57818d416a815197cfcab17bfe
Phuoc Le <phuocle@groovetechnology.com>
2019-07-24 09:24:53 +0700

    Goroutines, Channels example: Ping Pong

---
+++ week2/pingpong.go
@@ -0,0 +1,31 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main() {
+	var ping = make(chan int)
+	var pong = make(chan int)
+
+	go func () {
+		for {
+			<- ping
+			fmt.Println("ping")
+			pong <- 1
+		}
+	} ()
+
+	go func () {
+		for {
+			<- pong
+			fmt.Println("pong")
+			ping <- 1
+		}
+	} ()
+
+	ping <- 1
+
+	fmt.Scanln()
+    fmt.Println("done")
+}
