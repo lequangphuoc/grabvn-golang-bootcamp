983fa0b5c5d041dbf12bb5fbc01a0a335e109326
Phuoc Le <phuocle@groovetechnology.com>
2019-08-05 09:25:14 +0700

    assignment week 3

--- week3/assignment/client/client.go
+++ week3/assignment/client/client.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"time"
 
+	"google.golang.org/grpc"
+	pb "grabvn-golang-bootcamp/week3/assignment/passengerfeedback"
+)
+
+const (
+	address     = "localhost:50051"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewPassengerFeedbackServiceClient(conn)
+
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// Call Create
+	req1 := pb.AddFeedbackRequest{
+		Feedback: &pb.PassengerFeedback{
+			BookingCode: "BC001",
+			PassengerID: 1,
+			Feedback: "Very good",
+		},
+	}
+	rep1, err := c.AddFeedback(ctx, &req1)
+
+	log.Printf("%s", rep1.ResponseMessage)
 }
