27177a33c27172a3bd374fe605088ca5d5e64fe1
Phuoc Le <phuocle@groovetechnology.com>
2019-08-13 13:19:59 +0700

    Unit Test Todo

--- week4/assignment/todo/server/service/todo_test.go
+++ week4/assignment/todo/server/service/todo_test.go
@@ -22,3 +22,36 @@ func TestGetToDo(t *testing.T)  {
 	assert.Equal(t, expectedRes, res)
 	mockToDoRep.AssertExpectations(t)
 }
+
+func TestCreateTodo(t *testing.T) {
+	mockToDoRep := &mocks.ToDo{}
+	toDo := &pb.Todo{}
+	req := &pb.CreateTodoRequest{ Item: toDo}
+
+	mockToDoRep.On("Insert", req.Item).Return(nil)
+	service := ToDo{ToDoRepo: mockToDoRep}
+
+	res, err := service.CreateTodo(nil, req)
+
+	expectedRes := &pb.CreateTodoResponse{ Id: toDo.Id }
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedRes, res)
+	mockToDoRep.AssertExpectations(t)
+}
+
+func TestListToDo(t *testing.T)  {
+	mockToDoRep := &mocks.ToDo{}
+	toDo := &pb.Todo{}
+	req := &pb.ListTodoRequest{}
+	mockToDoRep.On("List", req).Return(toDo, nil)
+	service := ToDo{ToDoRepo: mockToDoRep}
+
+	res, err := service.ListTodo(nil, req)
+
+	expectedRes := &pb.GetTodoResponse{ Item: toDo}
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedRes, res)
+	mockToDoRep.AssertExpectations(t)
+}
